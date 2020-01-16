d563a5a7329b3b6b85d49f8be07d3d5c45e910f7
Christopher Aguiar <epicchris17@gmail.com>
2020-01-16 12:24:01 -0500

    It works but jasmine is being a creature and does not want it to work.

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,7 +1,59 @@
-function StackDataStructure () {
+// Stack class
+class Stack {
+  // Array is used to implement stack
+  constructor() {
+    this.items = [];
+  }
 
-    let stack = [];
-    stack.length = 10;
+  // push function
+  push(element) {
+    // push element into the items
+    this.items.push(element);
+  }
 
+  // pop function
+  pop() {
+    // return top most element in the stack
+    // and removes it from the stack
+    // Underflow if stack is empty
+    if (this.items.length == 0) {
+      return "Underflow";
+    }
+    return this.items.pop();
+  }
+
+  // peek function
+  peek() {
+    // return the top most element from the stack
+    // but does'nt delete it.
+    return this.items[this.items.length - 1];
+  }
+
+  // isEmpty function
+  isEmpty() {
+    // return true if stack is empty
+    return this.items.length == 0;
+  }
+
+  // printStack function
+  printStack() {
+    let str = "";
+    for (let i = 0; i < this.items.length; i++) {
+      str += this.items[i] + " ";
+    }
+    return str;
+  }
+}
+
+let stack = new Stack();
+
+function StackDataStructure() {
+
+    stack.push("element");
+    stack.push("element 2");
+    stack.pop();
 
 }
+
+
+
